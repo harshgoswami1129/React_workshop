aae0a7dc2789a3298940b49c66eb1e4a2f27aa26
hparvat610-collab <hparvat610@rku.ac.in>
2026-02-26 09:22:43 +0530

    First commit

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import process from "node:process";
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/workshop",
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import process from "node:process";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+// import registerRoutes from "./routes/registerRoutes.js";
+
+
+dotenv.config();
+
+
+import connectDB from "./config/database.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// import authRoutes from './routes/authRoutes.js';
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.use("/api/", registerRoutes);
+
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+// Connect to MongoDB
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1);
+  });
